1ebe2c1794164f04169cd8a848ef5d97de59ddf2
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-08-22 23:59:27 -0400

    Added main function

--- get_game_data.py
+++ get_game_data.py
@@ -4,8 +4,15 @@ import shutil   #   Copy/Overwrite
 from subprocess import PIPE, run    #   Allows Terminal functionalty
 import sys  #   Get access to command line arguments
 
+def main(source, target):
+    cwd = os.getcwrd()  #   Gets current Working Directory
+    source_path = os.path.join(cwd, source)
+
 if __name__ == "__main__":
     args = sys.argv #   Variable will store command line arguments
 
     if len(args) != 3:
         raise Exception("You must pass a source and target directory - only.")
+    
+    source, target = args[1:]
+    main(source, target)
