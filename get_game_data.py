be8d55e97d58fd634a07665ec159f9c800392121
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-08-23 00:10:22 -0400

    Added a method to retrieve all directories for games

--- get_game_data.py
+++ get_game_data.py
@@ -4,9 +4,24 @@ import shutil   #   Copy/Overwrite
 from subprocess import PIPE, run    #   Allows Terminal functionalty
 import sys  #   Get access to command line arguments
 
+GAME_DIR_PATTERN = "game"   #   Lookfor string "game" in any directory
+
+def find_all_game_paths(source):
+    game_paths = []
+    #os.walk() recursively goes through its arguments directiry and returns its root, its directories, and its files
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
+        break
+
+    return game_paths
+
 def main(source, target):
     cwd = os.getcwrd()  #   Gets current Working Directory
-    source_path = os.path.join(cwd, source)
+    source_path = os.path.join(cwd, source) #   Use os.path.join and never string cocat
+    target_path = os.path.join(cwd, target)
 
 if __name__ == "__main__":
     args = sys.argv #   Variable will store command line arguments
