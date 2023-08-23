34396b1a61c03d91766d00ebc22fc5626d94bd1d
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-08-23 00:52:37 -0400

    Added function to copy files into destination directory

--- get_game_data.py
+++ get_game_data.py
@@ -30,15 +30,38 @@ def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+def copy_and_overwrite(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(source, dest)
+
+def make_json_metadata_file(path, game_dirs):
+    data = {
+        "gameNames": game_dirs,
+        "numberOfGames": len(game_dirs)
+    }
+
+    with open(path, 'w') as f:
+        json.dump(data, f)
+
 def main(source, target):
     cwd = os.getcwd()  #   Gets current Working Directory
     source_path = os.path.join(cwd, source) #   Use os.path.join and never string cocat
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    new_game_dirs = get_name_from_paths(game_paths, "game")
+    new_game_dirs = get_name_from_paths(game_paths, "_game")
 
     create_dir(target)
+
+    for src, dest in zip(game_paths, new_game_dirs):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
+    
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path, new_game_dirs)
+
+    
 
 
 
