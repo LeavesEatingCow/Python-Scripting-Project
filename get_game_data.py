5e41dc94bc56d2fa6ba8ef590a9a19825bb87209
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-08-22 23:52:32 -0400

    Added Command Line Accessability

--- get_game_data.py
+++ get_game_data.py
@@ -3,3 +3,9 @@ import json #   JSON Files
 import shutil   #   Copy/Overwrite
 from subprocess import PIPE, run    #   Allows Terminal functionalty
 import sys  #   Get access to command line arguments
+
+if __name__ == "__main__":
+    args = sys.argv #   Variable will store command line arguments
+
+    if len(args) != 3:
+        raise Exception("You must pass a source and target directory - only.")
